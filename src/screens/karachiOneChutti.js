1cc5e3c21d4fb987cb4901eee2d8dd3d6477e8aa
Owais Ahmed <muhammadowais25122003@gmail.com>
2023-04-06 02:33:05 +0500

    unwanted data remove

--- src/screens/karachiOneChutti.js
+++ src/screens/karachiOneChutti.js
@@ -3,19 +3,12 @@ import React, { useEffect, useState } from 'react';
 import { responsiveScreenFontSize, responsiveWidth } from 'react-native-responsive-dimensions';
 import { responsiveHeight } from 'react-native-responsive-dimensions';
 import { responsiveFontSize } from 'react-native-responsive-dimensions';
-import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
-import storage from '@react-native-firebase/storage';
 import firestore from '@react-native-firebase/firestore';
-import { firebase } from '@react-native-firebase/firestore';
 const devicewidth = Dimensions.get('window').width;
 const deviceheight = Dimensions.get('window').height;
 
 const KarachiOneChutti = () => {
-    const [khi1qafila, setkhi1qafila] = useState('')
     const [khi1chutti, setkhi1chutti] = useState('')
-    const [khi1darussunnah, setkhi1darussunnah] = useState('')
-    const [khi1Infiradi, setkhi1Infiradi] = useState('')
-
 
     useEffect(() => {
         let ChuttiNameData = [];
@@ -38,50 +31,7 @@ const KarachiOneChutti = () => {
                     setkhi1chutti(NewData);
                 }
             });
-        const qafila = firestore()
-            .collection('users')
-            .where('Group', '==', 'Karachi 1')
-            .where('Status', '==', "مدنی قافلہ")
-            .onSnapshot(querySnapshot => {
-                const qafilaData = [];
-                querySnapshot.forEach(documentSnapshot => {
-                    qafilaData.push({
-                        id: documentSnapshot.id,
-                        ...documentSnapshot.data(),
-                    });
-                });
-                setkhi1qafila(qafilaData.length);
-            });
-        const Darussunnah = firestore()
-            .collection('users')
-            .where('Group', '==', 'Karachi 1')
-            .where('Status', '==', "دار السنہ")
-            .onSnapshot(querySnapshot => {
-                const DarussunnahData = [];
-                querySnapshot.forEach(documentSnapshot => {
-                    DarussunnahData.push({
-                        id: documentSnapshot.id,
-                        ...documentSnapshot.data(),
-                    });
-                });
-                setkhi1darussunnah(DarussunnahData.length);
-            });
-        const infiradi = firestore()
-            .collection('users')
-            .where('Group', '==', 'Karachi 1')
-            .where('Status', '==', "انفرادی جدول")
-            .onSnapshot(querySnapshot => {
-                const infiradiData = [];
-                querySnapshot.forEach(documentSnapshot => {
-                    infiradiData.push({
-                        id: documentSnapshot.id,
-                        ...documentSnapshot.data(),
-                    });
-                });
-                setkhi1Infiradi(infiradiData.length);
-            });
     }, [])
-    // console.log(khi1chutti);
     return (
         <View style={styles.main}>
             {khi1chutti != '' ? (
@@ -110,23 +60,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     FlatListVIew: {
-        width: responsiveWidth(90),
-        // height:responsiveHeight(50),
-        // backgroundColor:'blue'
+        width: responsiveWidth(90)
     },
     DataView: {
         backgroundColor: "#135229",
         borderRadius: 10,
-        // paddingHorizontal:responsiveWidth(2),
         paddingVertical: responsiveHeight(1),
         marginVertical: responsiveHeight(0.5),
     },
     Name: {
         color: 'white',
         fontSize: responsiveFontSize(2),
-        // marginBottom:8,
-        // marginTop:responsiveHeight(2),
-
         textAlign: 'center'
     },
     Phone: {
