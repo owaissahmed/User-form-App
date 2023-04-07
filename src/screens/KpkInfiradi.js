f87f329d85388bf3c6ab5bdf92162db8eca564cd
Owais Ahmed <muhammadowais25122003@gmail.com>
2023-04-08 01:43:56 +0500

    Kpk Start

---
+++ src/screens/KpkInfiradi.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const KpkInfiradi = () => {
+  return (
+    <View>
+      <Text>KpkInfiradi</Text>
+    </View>
+  )
+}
+
+export default KpkInfiradi
+
+const styles = StyleSheet.create({})
